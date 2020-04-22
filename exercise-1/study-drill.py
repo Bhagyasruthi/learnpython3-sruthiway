398f99911b942e26bbd7d30471a2703b6241295a
Bhagyasruthi <bhagytirumalasetty@gmail.com>
2020-04-22 23:55:11 +0100

    First commit - exercise 1, 2 and 3

---
+++ exercise-1/study-drill.py
@@ -0,0 +1,11 @@
+print("hello world")
+print("Hello again")
+print("I like typing this")
+print("This is fun")
+print("Yay! printing")
+print("I'd much rather you not")
+print("I said do not touch this")
+
+#make script another line 
+print("Study drill 1 -> Printing another line")   # this is another comment 
+# This character helps to print comments 
---
+++ exercise-3/exercise-3.py
@@ -0,0 +1,18 @@
+
+print("I will now count my chickens:")
+#print the number of  by PEMDAS math operations
+print("Hens", 25+30/6 )
+print("Roosters", 100-25*3%4)
+print("now i will cpount the eggs:")
+print(3+2+1-5+4%2-1/4+6)
+print("is it true that 3+2<5-7?")
+print(3+2<5-7)
+print("what is 3+2?", 3+2)
+print("what is 5-7?", 5-7)
+print("Oh, thats why its false")
+print("How about some more")
+print("is it greater?", 5>-2)
+print("Is it greater or equals?", 5>=-2)
+print("Is it less or equals?",5<=-2)
+#used floating point values 
+print("sruhti",25+4/2-3*6.1)
